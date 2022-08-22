2fd44f9b365ffc28ef995d4b5e9cc04315872c5e
Barsha Chaudhary <barsha.chaudhary@bison.howard.edu>
2022-08-21 22:09:08 -0400

    Added additional feature

--- app/src/main/java/com/example/prework/MainActivity.kt
+++ app/src/main/java/com/example/prework/MainActivity.kt
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lateinit var  string: String
         val btnHello = findViewById<Button>(R.id.btnClickForToast)
+        var etName: EditText = findViewById<EditText>(R.id.etName)
         btnHello.setOnClickListener {
-            Log.v("Hello World","Hello World")
-            Toast.makeText(this, "Hello to you!", Toast.LENGTH_SHORT).show()
+            string = etName.text.toString();
+            Log.v("Hello World", "Hello $string");
+            Toast.makeText(this, "Hello $string", Toast.LENGTH_SHORT).show()
         }
     }
 }
